48982d8c2ddb8d746341c0f4ad7b5913889ad67b
Kristjan Jansen <kristjan.jansen@gmail.com>
2024-06-11 21:44:12 +0300

    fix

--- utils/site.ts
+++ utils/site.ts
@@ -1,19 +1,13 @@
-const key = "green";
+const key = "lv_main";
 
 const sites = [
-  {
-    key: "ee_main",
-    locales: ["et", "en"],
-    defaultLocale: "et",
-    name: "Enefit Estonia",
-    segments: { household: "", business: "ari" },
-  },
   {
     key: "lv_main",
     locales: ["lv", "ru", "en"],
     defaultLocale: "lv",
     name: "Enefit Latvia",
     segments: { household: "", business: "biznesam" },
+    news: "majai/zinas",
   },
   {
     key: "lt_main",
@@ -21,20 +15,31 @@ const sites = [
     defaultLocale: "lt",
     name: "Enefit Lietuva",
     segments: { household: "", business: "verslui" },
+    news: "privatiems/naujienos",
   },
   {
     key: "pl_main",
     locales: ["pl"],
     defaultLocale: "pl",
-    name: "Enefit Poland",
-    segments: { household: "" },
+    name: "Enefit Polska",
+    segments: { business: "" },
+    news: "aktualnosci",
   },
   {
     key: "green",
     locales: ["et", "en", "ru", "lv", "lt"],
     defaultLocale: "et",
     name: "Enefit Green",
+    segments: { corporate: "" },
+    news: "uudised",
+  },
+  {
+    key: "volt",
+    locales: ["et", "en", "ru"],
+    defaultLocale: "et",
+    name: "Enefit Volt",
     segments: { household: "" },
+    blog: "blogi",
   },
 ];
 
